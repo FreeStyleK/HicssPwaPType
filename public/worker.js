709c350b6b76c2ae0769599b033f536c8cfecf75
khlim <khlim@harami.co.kr>
2020-07-03 09:09:15 +0900

    worker 첫 active가 안 되는 현상 수정

--- public/worker.js
+++ public/worker.js
@@ -1,51 +1,76 @@
-var CACHE_NAME = 'pwa-task-manager';
+/*
+ * @license
+ * Your First PWA Codelab (https://g.co/codelabs/pwa)
+ * Copyright 2019 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+"use strict";
+
+// CODELAB: Update cache names any time any of the cached files change.
+const CACHE_NAME = "my-site-cache-v1";
+
 var urlsToCache = [
-    '/'
+  "/",
+  // ,'/styles/inline.css'
+  // ,'/index.html'
 ];
-// Install a service worker
-self.addEventListener('install', event => {
-    // Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(function(cache) {
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
-// Cache and return requests
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-        .then(function(response) {
-            // Cache hit - return response
-            if (response) {
-                return response;
-            }
-            return fetch(event.request);
-        })
-    );
-});
-// Update a service worker
-self.addEventListener('activate', event => {
-    var cacheWhitelist = ['pwa-task-manager'];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+
+// CODELAB: Add list of files to cache here.
+const FILES_TO_CACHE = ["/offline.html"];
+
+///////////////////////////////////////////////////////////////////////////////////
+
+self.addEventListener("install", (evt) => {
+  console.log("[ServiceWorker] Install");
+  // CODELAB: Precache static resources here.
+  // FreeStyle
+  evt.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        console.log("[CACHE OPEN]");
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => {
+        return self.skipWaiting();
+      })
+  );
 });
 
-self.addEventListener('notificationclick', function (event) {
-    log('Push clicked');
-    event.notification.close();
-    event.waitUntil(
-        clients.openWindow('https://github.com/leegeunhyeok/pwa-example')
-    );
+self.addEventListener("activate", (event) => {
+  var cacheWhitelist = ["pwa-task-manager"];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
 });
